b54097b525449df51cf48148aeca2dadccebe8f2
sagorchowdhury <rajchowdhury820@gmail.com>
2026-02-04 02:46:32 +0400

    added js

---
+++ recap.js
@@ -0,0 +1,32 @@
+// const numbers = [10, 20, 30, 40, 50];
+
+const friends = ['sagy', 'onontho', 'raj'];
+// console.log(friends);
+// console.log(friends.length); /ans -3
+
+// console.log(friends[1]='rocky');
+// console.log(friends); /ans- [ 'sagy', 'rocky', 'raj' ]
+
+// console.log(friends.push('bala'));
+// console.log(friends);  /ans- [ 'sagy', 'onontho', 'raj', 'bala' ]
+
+// console.log(friends.pop());
+// console.log(friends); ans - [ 'sagy', 'onontho']
+
+
+// console.log(friends.shift());
+// console.log(friends); ans - ['onontho', "raj"]
+
+
+// console.log(friends.unshift('boki'));
+// console.log(friends);  ans - ['boki','sagy','onontho', "raj"]
+
+
+
+// todo loops
+
+const numbers = [10, 20, 30, 40, 50];
+for (const num of numbers) {
+    console.log(num)
+}
+

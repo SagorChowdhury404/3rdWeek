bdd1060c41eae16d86e5f45abcb0cf54fd2a705c
sagorchowdhury <rajchowdhury820@gmail.com>
2026-02-06 17:51:16 +0400

    added codes

--- function.js
+++ function.js
@@ -1,7 +1,22 @@
 const number = 45;
-function leCode() {
-    console.log('lets go for code')
+// function leCode(){
+//     console.log('lets go for code')
+// }
+// leCode();
+
+
+// function leCode(num) {
+//     console.log(num, 'lets go for code')
+// }
+// leCode(5);
+
+function leCode(num1, num2) {
+    console.log(num1 + num2)
 }
-leCode();
+leCode(5, 6);
+
+
+
+
 
 
